dd93dfb59a29cec3b2be44495cbf88bb4fff3910
zhuqiang <zhuqiang@51mjs.com>
2018-09-11 17:38:17 +0800

    增加多环境配置

--- src/main/java/cn/cpic/insurance/web/controller/TestRestController.java
+++ src/main/java/cn/cpic/insurance/web/controller/TestRestController.java
@@ -7,11 +7,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController()
 @RequestMapping("/test")
