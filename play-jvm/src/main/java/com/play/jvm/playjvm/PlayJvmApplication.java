e48d08ec86d6895a84251c17f3608f1f8bd08587
whats <woya2010@126.com>
2020-04-30 13:52:24 +0800

    init project

---
+++ play-jvm/src/main/java/com/play/jvm/playjvm/PlayJvmApplication.java
@@ -0,0 +1,13 @@
+package com.play.jvm.playjvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayJvmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayJvmApplication.class, args);
+    }
+
+}
---
+++ play-jvm/src/test/java/com/play/jvm/playjvm/PlayJvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.play.jvm.playjvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayJvmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
